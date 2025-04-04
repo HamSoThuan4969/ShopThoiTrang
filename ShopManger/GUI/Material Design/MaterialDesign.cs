392fce294e1dacce545f16fde707ea387faee7e8
HamSoThuan4969 <kienan612004@gmail.com>
2025-04-04 18:04:28 +0700

    Add files via upload

---
+++ ShopManger/GUI/Material Design/MaterialDesign.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;  
+using System.Threading.Tasks;
+using MaterialSkin;
+using MaterialSkin.Controls;
+using System.Windows.Forms;
+
+namespace ShopManger.GUI.MaterialDesign
+{
+    public class MaterialTheme
+    {
+        public static void ApplyTheme(Form form)
+        {
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(form as MaterialForm);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(
+                Primary.Blue600, Primary.Blue700,
+                Primary.Blue200, Accent.LightBlue200,
+                TextShade.WHITE
+            );
+        }
+    }
+}
