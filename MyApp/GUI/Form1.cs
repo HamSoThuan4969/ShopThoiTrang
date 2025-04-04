bff32769e911437f38da31933da5be95405bd527
HamSoThuan <2251012002an@ou.edu.vn>
2025-04-04 21:35:03 +0700

    demo1

---
+++ MyApp/GUI/Form1.cs
@@ -0,0 +1,30 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace GUI
+{
+    public partial class Form1 : MaterialForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeMaterialSkin();
+        }
+
+        private void InitializeMaterialSkin()
+        {
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(
+                Primary.Blue600, Primary.Blue700,
+                Primary.Blue200, Accent.LightBlue200,
+                TextShade.WHITE);
+        }
+
+        private void materialRadioButton1_CheckedChanged(object sender, System.EventArgs e)
+        {
+
+        }
+    }
+}
