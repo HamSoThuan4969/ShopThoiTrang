0e348855c10b4335480f10f699a2e30b1a0d1fe8
huy vo <votanhuyy1504@gmail.com>
2025-05-11 16:39:22 +0700

    Add ScheduleBLL.cs

---
+++ MyApp/BLL/ScheduleBLL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using DTO;
+
+namespace BLL
+{
+    public class ScheduleBLL
+    {
+        private RequestScheduleDAL dal = new RequestScheduleDAL();
+
+        public void CreateRequest(RequestSchedulesDTO req)
+        {
+            // Có thể kiểm tra hợp lệ, xử lý logic trước khi gọi DAL
+            dal.AddRequest(req);
+        }
+
+        public List<RequestSchedulesDTO> GetByDate(DateTime date)
+        {
+            return new RequestScheduleDAL().GetRequestsByDate(date);
+        }
+
+        public List<RequestSchedulesDTO> GetAll()
+        {
+            return dal.GetAllRequests();  // Gọi DAL để lấy danh sách
+        }
+    }
+}
